1a08883cb96543912f89e29388aae74ee0d57dc1
Dinh Tuan Dat <dtuandat2004@gmail.com>
2023-12-09 00:08:18 +0700

    delete mui v4

--- chat-client/src/components/Compose/Compose.tsx
+++ chat-client/src/components/Compose/Compose.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import './Compose.css';
-import Button from "@material-ui/core/Button";
-
-
+import Button from '@mui/material/Button';
 
 export default function Compose() {
     return (
--- chat-client/src/components/InfoList/InfoList.tsx
+++ chat-client/src/components/InfoList/InfoList.tsx
@@ -1,7 +1,7 @@
 import Toolbar from "../Toolbar/Toolbar";
 import './InfoList.css';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import IconButton from "@material-ui/core/IconButton";
+import IconButton from '@mui/material/IconButton';
 
 export default function InfoList() {
     var name = "LOREM"
