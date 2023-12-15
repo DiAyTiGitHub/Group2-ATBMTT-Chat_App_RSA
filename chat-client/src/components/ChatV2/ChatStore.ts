b815f31f5c655bef8a6e3978b9ed451633ddd397
Dinh Tuan Dat <dtuandat2004@gmail.com>
2023-12-16 01:25:20 +0700

    update source code

--- chat-client/src/components/ChatV2/ChatStore.ts
+++ chat-client/src/components/ChatV2/ChatStore.ts
@@ -6,158 +6,161 @@ import LocalStorage from "src/common/LocalStorage";
 import { getAllJoinedRooms } from "src/services/UserService";
 
 class ChatStore {
-  stompClient = null;
+    stompClient = null;
 
-  setStompClient = (stompClient: any) => {
-    this.stompClient = stompClient;
-  };
+    setStompClient = (stompClient: any) => {
+        this.stompClient = stompClient;
+    };
 
-  constructor() {
-    makeAutoObservable(this);
-  }
-
-  searchConversation = async (keyword: string) => {
-    try {
-      const searchObject = {
-        keyword: keyword,
-      };
-    } catch (err) {
-      console.log(err);
-      toast.error("Find conversation errors :( Please try again!");
-      throw new Error(err);
-    }
-  };
-
-  sendMessage = (messageContent: string) => {
-    if (!messageContent || messageContent.length === 0) {
-      return;
-    }
-    try {
-      const currentUser = LocalStorage.getLoginUser();
-
-      const chatMessage = {
-        content: this.encryptRSA(messageContent),
-        room: { id: this.chosenRoom.id },
-        messageType: { name: "chat" },
-        user: currentUser,
-      };
-      this.stompClient.send(
-        "/app/privateMessage",
-        {},
-        JSON.stringify(chatMessage)
-      );
-    } catch (err) {
-      console.log(err);
-      toast.error("Error occured when sending message, please try again :(");
-      throw new Error(err);
-    }
-  };
-  mod = (a, b, n) => {
-    if (n === 1) return 0;
-    let kq = 1;
-    for (let i = 0; i < b; i++) {
-      kq = (kq * a) % n;
-    }
-    return kq;
-  };
-  // Mã hoá
-  encryptRSA = (messageContent) => {
-    let q = 11,
-      p = 13,
-      e = 97;
-    let n = p * q;
-    let encryptedChar = [];
-    for (let i = 0; i < messageContent.length; i++) {
-      let charCode = messageContent.charCodeAt(i);
-      encryptedChar[i] = this.mod(charCode, e, n);
-    }
-    let encryptedString = String.fromCharCode(...encryptedChar);
-    console.log(btoa(encryptedString));
-    return btoa(encryptedString);
-  };
-  registerUser = () => {
-    this.connect();
-  };
-
-  connect = () => {
-    let Sock = new SockJS("http://localhost:8000/ws");
-    this.stompClient = over(Sock);
-    this.stompClient.connect({}, this.onConnected, this.onError);
-  };
-
-  onConnected = () => {
-    const currenUser = LocalStorage.getLoginUser();
-    this.stompClient.subscribe(
-      "/user/" + currenUser.id + "/privateMessage",
-      this.onReceiveRoomMessage
-    );
-  };
-
-  onError = (err: any) => {
-    console.log(err);
-    toast.error("Connect to chat server error, please try again!");
-  };
-
-  chosenRoom = null;
-  setChosenRoom = (chosenRoom: any) => {
-    this.chosenRoom = chosenRoom;
-  };
-
-  joinedRooms = [];
-  getAllJoinedRooms = async () => {
-    console.log("getAllJoinedRooms is called");
-    try {
-      const { data } = await getAllJoinedRooms();
-      this.joinedRooms = data;
-      this.chosenRoom = data[0];
-
-      if (!this.stompClient) {
-        toast.error(
-          "You haven't connected to chat server! Please login again!"
-        );
-        return;
-      }
-    } catch (error) {
-      console.log(error);
-      toast.error("Load conversation fail, please try again!");
-    }
-  };
-
-  onReceiveRoomMessage = (payload: any) => {
-    const payloadData = JSON.parse(payload.body);
-    const roomId = payloadData?.room?.id;
-    if (!roomId) {
-      toast.error("Received message errors!");
-      return;
+    constructor() {
+        makeAutoObservable(this);
     }
 
-    let isExisted = null;
-    for (let i = 0; i < this.joinedRooms.length; i++) {
-      const currentRoom = this.joinedRooms[i];
-      if (currentRoom.id === roomId) {
-        currentRoom.messages.push(payloadData);
-        this.joinedRooms[i] = { ...currentRoom };
-        isExisted = i;
-
-        if (currentRoom.id === this.chosenRoom.id) {
-          this.chosenRoom = { ...currentRoom };
+    searchConversation = async (keyword: string) => {
+        try {
+            const searchObject = {
+                keyword: keyword,
+            };
+        } catch (err) {
+            console.log(err);
+            toast.error("Find conversation errors :( Please try again!");
+            throw new Error(err);
         }
-      }
-    }
+    };
 
-    if (isExisted || isExisted == 0) {
-      const temp = this.joinedRooms[isExisted];
-      for (let i = isExisted; i >= 1; i--) {
-        this.joinedRooms[i] = this.joinedRooms[i - 1];
-      }
-      this.joinedRooms[0] = temp;
-      this.joinedRooms = [...this.joinedRooms];
-    } else {
-      const newRoom = payloadData.room;
-      this.joinedRooms.unshift(newRoom);
-      this.joinedRooms = [...this.joinedRooms];
-    }
-  };
+    sendMessage = (messageContent: string) => {
+        if (!messageContent || messageContent.length === 0) {
+            return;
+        }
+        try {
+            const currentUser = LocalStorage.getLoginUser();
+
+            const chatMessage = {
+                content: this.encryptRSA(messageContent),
+                room: { id: this.chosenRoom.id },
+                messageType: { name: "chat" },
+                user: currentUser,
+            };
+            this.stompClient.send(
+                "/app/privateMessage",
+                {},
+                JSON.stringify(chatMessage)
+            );
+        } catch (err) {
+            console.log(err);
+            toast.error("Error occured when sending message, please try again :(");
+            throw new Error(err);
+        }
+    };
+    
+    mod = (a, b, n) => {
+        if (n === 1) return 0;
+        let kq = 1;
+        for (let i = 0; i < b; i++) {
+            kq = (kq * a) % n;
+        }
+        return kq;
+    };
+
+    // Mã hoá
+    encryptRSA = (messageContent) => {
+        let q = 11,
+            p = 13,
+            e = 97;
+        let n = p * q;
+        let encryptedChar = [];
+        for (let i = 0; i < messageContent.length; i++) {
+            let charCode = messageContent.charCodeAt(i);
+            encryptedChar[i] = this.mod(charCode, e, n);
+        }
+        let encryptedString = String.fromCharCode(...encryptedChar);
+        console.log(btoa(encryptedString));
+        return btoa(encryptedString);
+    };
+
+    registerUser = () => {
+        this.connect();
+    };
+
+    connect = () => {
+        let Sock = new SockJS("http://localhost:8000/ws");
+        this.stompClient = over(Sock);
+        this.stompClient.connect({}, this.onConnected, this.onError);
+    };
+
+    onConnected = () => {
+        const currenUser = LocalStorage.getLoginUser();
+        this.stompClient.subscribe(
+            "/user/" + currenUser.id + "/privateMessage",
+            this.onReceiveRoomMessage
+        );
+    };
+
+    onError = (err: any) => {
+        console.log(err);
+        toast.error("Connect to chat server error, please try again!");
+    };
+
+    chosenRoom = null;
+    setChosenRoom = (chosenRoom: any) => {
+        this.chosenRoom = chosenRoom;
+    };
+
+    joinedRooms = [];
+    getAllJoinedRooms = async () => {
+        console.log("getAllJoinedRooms is called");
+        try {
+            const { data } = await getAllJoinedRooms();
+            this.joinedRooms = data;
+            this.chosenRoom = data[0];
+
+            if (!this.stompClient) {
+                toast.error(
+                    "You haven't connected to chat server! Please login again!"
+                );
+                return;
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error("Load conversation fail, please try again!");
+        }
+    };
+
+    onReceiveRoomMessage = (payload: any) => {
+        const payloadData = JSON.parse(payload.body);
+        const roomId = payloadData?.room?.id;
+        if (!roomId) {
+            toast.error("Received message errors!");
+            return;
+        }
+
+        let isExisted = null;
+        for (let i = 0; i < this.joinedRooms.length; i++) {
+            const currentRoom = this.joinedRooms[i];
+            if (currentRoom.id === roomId) {
+                currentRoom.messages.push(payloadData);
+                this.joinedRooms[i] = { ...currentRoom };
+                isExisted = i;
+
+                if (currentRoom.id === this.chosenRoom.id) {
+                    this.chosenRoom = { ...currentRoom };
+                }
+            }
+        }
+
+        if (isExisted || isExisted == 0) {
+            const temp = this.joinedRooms[isExisted];
+            for (let i = isExisted; i >= 1; i--) {
+                this.joinedRooms[i] = this.joinedRooms[i - 1];
+            }
+            this.joinedRooms[0] = temp;
+            this.joinedRooms = [...this.joinedRooms];
+        } else {
+            const newRoom = payloadData.room;
+            this.joinedRooms.unshift(newRoom);
+            this.joinedRooms = [...this.joinedRooms];
+        }
+    };
 }
 
 export default ChatStore;
