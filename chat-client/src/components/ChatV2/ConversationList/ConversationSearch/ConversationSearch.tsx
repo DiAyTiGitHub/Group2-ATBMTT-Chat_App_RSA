123312189988174961c042bd25b0a43410fce3da
unknown <1pho2trung@gmail.com>
2023-12-20 23:16:04 +0700

    new group chat

--- chat-client/src/components/ChatV2/ConversationList/ConversationSearch/ConversationSearch.tsx
+++ chat-client/src/components/ChatV2/ConversationList/ConversationSearch/ConversationSearch.tsx
@@ -2,10 +2,17 @@ import React, { memo, useState } from 'react';
 import './ConversationSearch.css';
 import { observer } from 'mobx-react';
 import { useStore } from 'src/stores';
+import { IconButton, Modal, Box, List, ListItem, ListItemAvatar, ListItemButton, ListItemText, Checkbox, Avatar, Typography, Divider, TextField, Button } from '@mui/material';
+import GroupAddIcon from '@mui/icons-material/GroupAdd';
 
 function ConversationSearch() {
-  const { chatStore } = useStore();
+  const { chatStore, friendsStore } = useStore();
   const { searchConversation } = chatStore;
+  const { usersList } = friendsStore;
+  console.log(usersList);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   
   const [searchKeyword, setSearchKeyword] = useState();
 
@@ -20,11 +27,31 @@ function ConversationSearch() {
   }
 
   function handleOnKeyDown(event: any) {
-    // Check if the key pressed is Enter (key code 13)
     if (event.key === "Enter") {
-      // Your code to handle Enter key press goes here
       handleSearch(searchKeyword);
     }
+  }
+
+  const [checked, setChecked] = React.useState([1]);
+
+  const handleToggle = (value: number) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  function renderAvatar(avatar) {
+    if (!avatar || avatar.trim() === '') {
+      return 'https://www.treasury.gov.ph/wp-content/uploads/2022/01/male-placeholder-image.jpeg';
+    }
+    return avatar;
   }
 
   return (
@@ -37,6 +64,53 @@ function ConversationSearch() {
         onChange={handleChange}
         onKeyDown={handleOnKeyDown}
       />
+      <IconButton aria-label="new group chat" onClick={handleOpen}>
+        <GroupAddIcon />
+      </IconButton>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box className='modal-container'>
+          <Typography variant='h5' sx={{ fontWeight: 800}}>NEW GROUP CHAT</Typography>
+          <TextField id="standard-basic" label="Enter chat name" variant="standard" />
+          <List dense sx={{ width: '100%' }}>
+            {usersList.map((user, index) => {
+              const labelId = `checkbox-list-secondary-label-${index}`;
+              return (
+                <ListItem
+                  key={index}
+                  secondaryAction={
+                    <Checkbox
+                      edge="end"
+                      onChange={handleToggle(index)}
+                      checked={checked.indexOf(index) !== -1}
+                      inputProps={{ 'aria-labelledby': labelId }}
+                    />
+                  }
+                  disablePadding
+                >
+                  <ListItemButton>
+                    <ListItemAvatar>
+                      <Avatar
+                        alt=''
+                        src={ renderAvatar(user.avatar) }
+                      />
+                    </ListItemAvatar>
+                    <ListItemText id={labelId} primary={user.username} color='black'/>
+                  </ListItemButton>
+                </ListItem>
+              );
+            })}
+          </List>
+          <div className='d-flex justify-around'>
+            <Button variant="contained">Cancel</Button>
+            <Button variant="contained">Confirm</Button>
+          </div>
+        </Box>
+      </Modal>
     </div>
   );
 }
