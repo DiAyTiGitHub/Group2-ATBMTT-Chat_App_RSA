8943a2fa9fb26afcd2ef628ccbba80c10b04e545
Nathan10252 <hoangvuong10252@gmail.com>
2023-12-30 02:41:21 +0700

    update infoListLoading

--- chat-client/src/components/ChatV2/InfoList/InfoListLoadingSkeleton.tsx
+++ chat-client/src/components/ChatV2/InfoList/InfoListLoadingSkeleton.tsx
@@ -4,7 +4,12 @@ import React, { memo } from "react";
 
 function LoadingSkeleton() {
     return (
-        <Skeleton animation="wave" variant="circular" width={120} height={120} className="pr-3" />
+        <>
+            <Skeleton animation="wave" variant="circular" width={150} height={150} className="pr-3 mt-3" />
+            <Skeleton animation="wave" variant="text" width={100} height={35} className="mt-2" />
+            <Skeleton animation="wave" variant="text" width={50} height={35} />
+            <Skeleton animation="wave" variant="text" width={120} height={50} />
+        </>
     );
 }
 
