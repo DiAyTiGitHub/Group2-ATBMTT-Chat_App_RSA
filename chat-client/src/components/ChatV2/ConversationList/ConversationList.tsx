efbec31d016e2667cb93ebd0552540639c82ae5c
unknown <1pho2trung@gmail.com>
2023-12-30 22:18:51 +0700

    convo scroll

--- chat-client/src/components/ChatV2/ConversationList/ConversationList.tsx
+++ chat-client/src/components/ChatV2/ConversationList/ConversationList.tsx
@@ -16,51 +16,53 @@ function ConversationList() {
   } = chatStore;
 
   return (
-    <div className="conversation-list flex-column">
+    <div>
       <Toolbar title="Chat">
-        <ConversationSearch />
-      </Toolbar>
-
-      <div className="conversationListItemWrapper">
-        {
-          // if is loading, render skeleton
-          isLoading ? (
-            <>
-              {
-                [1, 2, 3, 4, 5, 6, 7].map(function (_, index) {
-                  return (
-                    <ConversationListItemLoadingSkeleton key={index} />
-                  );
-                })
-              }
-            </>
-          )
-            :
-            // if is not loading, render data
-            (
+          <ConversationSearch />
+        </Toolbar>
+      <div className="conversation-list flex-column">
+        <div className="conversationListItemWrapper">
+          {
+            // if is loading, render skeleton
+            isLoading ? (
               <>
                 {
-                  joinedRooms.map(function (room, index) {
+                  [1, 2, 3, 4, 5, 6, 7].map(function (_, index) {
                     return (
-                      <ConversationListItem
-                        key={index}
-                        room={room}
-                      />
+                      <ConversationListItemLoadingSkeleton key={index} />
                     );
                   })
                 }
-                {
-                  ((!joinedRooms || (joinedRooms.length === 0)) && !isLoading) && (
-                    <div className="no-conversation">
-                      <p>You don't have any conversation, lets add friends and start chatting!</p>
-                    </div>
-                  )
-                }
               </>
             )
-        }
+              :
+              // if is not loading, render data
+              (
+                <>
+                  {
+                    joinedRooms.map(function (room, index) {
+                      return (
+                        <ConversationListItem
+                          key={index}
+                          room={room}
+                        />
+                      );
+                    })
+                  }
+                  {
+                    ((!joinedRooms || (joinedRooms.length === 0)) && !isLoading) && (
+                      <div className="no-conversation">
+                        <p>You don't have any conversation, lets add friends and start chatting!</p>
+                      </div>
+                    )
+                  }
+                </>
+              )
+          }
+        </div>
       </div>
     </div>
+
   );
 }
 
