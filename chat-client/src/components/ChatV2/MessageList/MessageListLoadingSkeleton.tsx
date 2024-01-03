482b3fcd74b1e24a06949db8dff7927b92f8d220
Nathan10252 <hoangvuong10252@gmail.com>
2024-01-03 23:30:02 +0700

    update skeleton

--- chat-client/src/components/ChatV2/MessageList/MessageListLoadingSkeleton.tsx
+++ chat-client/src/components/ChatV2/MessageList/MessageListLoadingSkeleton.tsx
@@ -1,46 +1,54 @@
-import { Skeleton } from "@mui/material";
+import { Skeleton, Stack } from "@mui/material";
 import { observer } from "mobx-react";
 import { memo } from "react";
 
 function LoadingSkeleton() {
     return (
         <>
-            <div className="flex align-center">
-                <Skeleton animation="wave" variant="circular" width={40} height={40} className="ml-1" />
-                <Skeleton animation="wave" variant="text" width={100} height={40} className="ml-2" />
+            <div className="flex align-center mt-4">
+                <Skeleton animation="wave" variant="circular" width={50} height={50} className="ml-1" />
+                <Skeleton animation="wave" variant="rounded" width={150} height={40} className="ml-2 br-4" />
             </div>
             <div className="flex justify-right">
-                <Skeleton animation="wave" variant="text" width={140} height={40} className="mr-2" />
+                <Skeleton animation="wave" variant="rounded" width={140} height={50} className="mr-2 br-4" />
             </div>
             <div className="flex">
-                <Skeleton animation="wave" variant="circular" width={40} height={40} className="ml-1" />
+                <Skeleton animation="wave" variant="circular" width={50} height={50} className="ml-1" />
                 <div className="display-block">
-                    <Skeleton animation="wave" variant="text" width={400} height={40} className="ml-2" />
-                    <Skeleton animation="wave" variant="text" width={400} height={80} className="ml-2" />
+                    <Skeleton animation="wave" variant="rounded" width={500} height={50} className="ml-2 brtl-4 brtr-4 brbr-4" />
+                    <Skeleton animation="wave" variant="rounded" width={500} height={80} className="ml-2 mt-1 brtr-4 brbr-4 brbl-4"/>
                 </div>
             </div>
             <div className="flex justify-right">
-                <Skeleton animation="wave" variant="text" width={400} height={80} className="mr-2" />
+                <Skeleton animation="wave" variant="rounded" width={400} height={80} className="mr-2 br-4"/>
             </div>
             <div className="flex align-center">
-                <Skeleton animation="wave" variant="circular" width={40} height={40} className="ml-1" />
-                <Skeleton animation="wave" variant="text" width={190} height={40} className="ml-2" />
+                <Skeleton animation="wave" variant="circular" width={50} height={50} className="ml-1" />
+                <Skeleton animation="wave" variant="rounded" width={190} height={40} className="ml-2 br-4"/>
             </div>
             <div className="flex justify-right">
-                <Skeleton animation="wave" variant="text" width={400} height={120} className="mr-2" />
+                <Skeleton animation="wave" variant="rounded" width={400} height={120} className="mr-2 br-4"/>
             </div>
             <div className="flex">
-                <Skeleton animation="wave" variant="circular" width={40} height={40} className="ml-1" />
+                <Skeleton animation="wave" variant="circular" width={50} height={50} className="ml-1" />
                 <div className="display-block">
-                    <Skeleton animation="wave" variant="text" width={400} height={40} className="ml-2" />
-                    <Skeleton animation="wave" variant="text" width={400} height={80} className="ml-2" />
+                    <Skeleton animation="wave" variant="rounded" width={400} height={40} className="ml-2 brtl-4 brtr-4 brbr-4"/>
+                    <Skeleton animation="wave" variant="rounded" width={450} height={80} className="ml-2 mt-1 brtr-4 brbr-4"/>
+                    <Skeleton animation="wave" variant="rounded" width={400} height={60} className="ml-2 mt-1 brtr-4 brbr-4 brbl-4"/>
                 </div>
             </div>
             <div className="flex justify-right">
-                <Skeleton animation="wave" variant="text" width={400} height={40} className="mr-2" />
+                <Skeleton animation="wave" variant="rounded" width={400} height={40} className="mr-2 br-4"/>
+            </div>
+            <div className="flex">
+                <Skeleton animation="wave" variant="circular" width={50} height={50} className="ml-1" />
+                <div className="display-block">
+                    <Skeleton animation="wave" variant="rounded" width={400} height={40} className="ml-2 brtl-4 brtr-4 brbr-4" />
+                    <Skeleton animation="wave" variant="rounded" width={400} height={80} className="ml-2 mt-1 brtr-4 brbr-4 brbl-4"/>
+                </div>
             </div>
         </>
-        
+
     );
 }
 
